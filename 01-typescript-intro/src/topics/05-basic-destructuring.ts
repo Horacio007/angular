6194b0561f3fba8fb84d6c2822e451dd6495cfb0
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-07 09:18:35 -0600

    fin parte 21 destructuracion de arreglos

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -23,13 +23,20 @@ const audioPlayer: AudioPlayer = {
 const {song, song:anotherSong, details, details:{author:anotherAutor}} = audioPlayer;
 const { author} = details
 
-console.log('Song sacada con destructuracion del primer ejemplo:', song);
-console.log('Song sacada con destructuracion del segundo ejemplo:', anotherSong);
-console.log('Author sacado con destruccturacion de objetos de la forma separada:', author);
-console.log('Author sacado con destruccturacion de objetos de la forma todo junto:', anotherAutor);
-console.log('Song sacada normal:', audioPlayer.song);
-console.log('Song:', audioPlayer.songDuration);
-console.log('Author:', audioPlayer.details.author);
+// console.log('Song sacada con destructuracion del primer ejemplo:', song);
+// console.log('Song sacada con destructuracion del segundo ejemplo:', anotherSong);
+// console.log('Author sacado con destruccturacion de objetos de la forma separada:', author);
+// console.log('Author sacado con destruccturacion de objetos de la forma todo junto:', anotherAutor);
+// console.log('Song sacada normal:', audioPlayer.song);
+// console.log('Song:', audioPlayer.songDuration);
+// console.log('Author:', audioPlayer.details.author);
+
+// const dbz:string[] = ['Goku', 'Vegeta', 'Trunks'];
+const [, , trunks = 'Not found']:string[] = ['Goku', 'Vegeta'];
+// const trunks = dbz[3] || 'Personaje no encontrado';
+
+
+console.log('Personaje 3:', trunks);
 
 
 export {};

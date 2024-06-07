d5973ee2578c2a851b4df21cdbf7931055ad29ba
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-07 09:46:56 -0600

    fin de la clase 22 y 23 la cual es destructuracion de argumentos

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,47 @@
+interface Product {
+    description:string;
+    price:number;
+
+}
+
+const phone:Product = {
+    description: 'Nokia A1',
+    price: 150
+}
+
+const tablet:Product = {
+    description: 'iPadAir',
+    price:250.0
+}
+
+interface TaxCalculationOptions {
+    tax:number;
+    products:Product[];
+}
+
+// function taxCalculation(options:TaxCalculationOptions):[number, number]  {
+// function taxCalculation({tax, products}:TaxCalculationOptions):[number, number]  {
+function taxCalculation(options:TaxCalculationOptions):[number, number]  {
+    const {tax, products} = options;
+    
+    let total = 0;
+
+    products.forEach( ({price}) => {
+        total += price; 
+    });
+
+    return [total, total*tax];
+}
+
+const shoppingCart:Product[] = [phone, tablet];
+const tax:number = 0.15
+
+const [totalCalculated, taxTotal] = taxCalculation({
+    products: shoppingCart,
+    tax:tax
+});
+
+console.log('Total:', totalCalculated);
+console.log('Tax:', taxTotal);
+
+export {};

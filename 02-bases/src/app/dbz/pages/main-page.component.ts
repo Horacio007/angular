2d5e06d7dcbbf6c78a8d875b146dd792ab629497
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-11 17:21:27 -0600

    se termina clase 71 servicios

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,31 +9,7 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent  {
+  constructor (public dbzService:DbzService) {
 
-  public character:Character[] = [
-    {
-      name:'Krilin',
-      power: 500
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
-
-  onNewCharacter(character:Character):void {
-    console.log("Main Page");
-    console.log(character);
-    this.character.push(character);
-  }
-
-  onDeleteCharacter(idx:number):void {
-    console.log('desde main page');
-    console.log(idx);
-    this.character.splice(idx,1);
   }
 }
---
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+  public character:Character[] = [
+    {
+      name:'Krilin',
+      power: 500
+    },
+    {
+      name: 'Goku',
+      power: 9500
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ];
+
+  onNewCharacter(character:Character):void {
+    console.log("Main Page");
+    console.log(character);
+    this.character.push(character);
+  }
+
+  onDeleteCharacter(idx:number):void {
+    console.log('desde main page');
+    console.log(idx);
+    this.character.splice(idx,1);
+  }
+
+}

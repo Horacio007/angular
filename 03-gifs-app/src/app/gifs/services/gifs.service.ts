695de51d5d9ecbf4ba0519a666290e93f2738025
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-13 16:28:20 -0600

    se termina clase 96 poner tipo de dato a una respuesta http

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  public gifList:Gif[] = [];
 
   private _tagsHistory:string[] = [];
   private _apiKey:string = '8cnhBdMzYcTqS504AV0ioTRV3KoYcrZJ';
@@ -39,8 +42,11 @@ export class GifsService {
       .set('q', tag)
       .set('limit', 10);
 
-    this.http.get(`${this._serviceURL}/search?`, {params}).subscribe(response => {
-      console.log(response);
+    this.http.get<SearchResponse>(`${this._serviceURL}/search?`, {params}).subscribe(response => {
+      // console.log(response.data);
+      this.gifList = response.data;
+      console.log(this.gifList);
+      // console.log(response.jotdog);
     })
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=8cnhBdMzYcTqS504AV0ioTRV3KoYcrZJ&q=toreto&limit=10')
     // .then(response => response.json())

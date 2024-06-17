c3e3931516411037887bf6ec72cba1f8a7b83587
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-17 12:27:20 -0600

    se termina clase 126 manejo de errores

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, retry } from 'rxjs';
+import { Observable, tap, map, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -12,7 +12,16 @@ export class CountriesService {
 
   searchCapital(term:string):Observable<Country[]> {
     const url:string = `${this.apiURL}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url).pipe(
+      catchError(error => {
+        console.log(error);
+
+        return of([]);
+      })
+    //   tap(response => console.log('Paso por el tap 1', response)),
+    //   map(response => []),
+    //   tap(response => console.log('Paso por el tap 2', response))
+    );
   }
 
 }

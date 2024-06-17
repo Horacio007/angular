cc57a7ac28a90b67d2f9ce49e462fd051d76ee60
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-17 10:48:45 -0600

    se termina clase 122 funcionamiento del search box

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  searchByCapital(term:string):void {
+    console.log('desde by capital page ');
+    console.log({term});
+  }
+
 }

7406fc50ac36a76f3bc304a0d77661060ae4fb47
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-17 15:02:31 -0600

    se termina 129 debounce a la hora de dejar de escribir hacer peticion

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,14 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
+
+  private debouncer:Subject<string> = new Subject<string>();
 
   @Input()
   public placeHolder:string = '';
@@ -13,8 +16,25 @@ export class SearchBoxComponent {
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce:EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer.pipe(
+      debounceTime(500)
+    ).subscribe( r => {
+      this.onDebounce.emit(r);
+      console.log('deboiuncer value',r)
+    })
+  }
+
   searchElement(txt:string):void {
     this.onValue.emit(txt);
+  }
+
+  onKeyPress(searchTerm:string) {
+    this.debouncer.next(searchTerm);
+    // console.log(searchTerm);
   }
 
 }

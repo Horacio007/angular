d01d3744d41e3583abe8a0b13c8b7a26353ed998
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-17 14:26:22 -0600

    se termina clase 136 refacotiezacion en servicios

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, tap, map, catchError, of } from 'rxjs';
+import { Observable, tap, map, catchError, of, delay } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,18 @@ export class CountriesService {
   private apiURL:string="https://restcountries.com/v3.1";
 
   constructor(private http:HttpClient) { }
+
+  private getCountryRequest(url:string):Observable<Country[]> {
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => {
+          console.log(error);
+
+          return of([]);
+        }),
+        delay(2000)
+      );
+  }
 
   searchCountryByAlphaCode(code:string):Observable<Country | null> {
     const url:string = `${this.apiURL}/alpha/${code}`;
@@ -28,44 +40,26 @@ export class CountriesService {
 
   searchCapital(term:string):Observable<Country[]> {
     const url:string = `${this.apiURL}/capital/${term}`;
-    return this.http.get<Country[]>(url).pipe(
-      catchError(error => {
-        console.log(error);
-
-        return of([]);
-      })
+    return this.getCountryRequest(url);
     //   tap(response => console.log('Paso por el tap 1', response)),
     //   map(response => []),
     //   tap(response => console.log('Paso por el tap 2', response))
-    );
   }
 
   searchCountry(term:string):Observable<Country[]> {
     const url:string = `${this.apiURL}/name/${term}`;
-    return this.http.get<Country[]>(url).pipe(
-      catchError(error => {
-        console.log(error);
-
-        return of([]);
-      })
+    return this.getCountryRequest(url);
     //   tap(response => console.log('Paso por el tap 1', response)),
     //   map(response => []),
     //   tap(response => console.log('Paso por el tap 2', response))
-    );
   }
 
   searchRegion(region:string):Observable<Country[]> {
     const url:string = `${this.apiURL}/region/${region}`;
-    return this.http.get<Country[]>(url).pipe(
-      catchError(error => {
-        console.log(error);
-
-        return of([]);
-      })
+    return this.getCountryRequest(url);
     //   tap(response => console.log('Paso por el tap 1', response)),
     //   map(response => []),
     //   tap(response => console.log('Paso por el tap 2', response))
-    );
   }
 
 }

b6c002237d032c8f2f38672d09befae2a01ff756
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-26 11:24:48 -0600

    se termina 237 enlazar campos al formualrio reactivo

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
+
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+  });
+
+  onSave():void {
+    console.log(this.myForm.value);
+  }
 
 }

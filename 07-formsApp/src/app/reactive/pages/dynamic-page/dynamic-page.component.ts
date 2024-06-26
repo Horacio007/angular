b3f1b086f6592a1775170722677f253ee4d1f9a1
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-26 14:01:32 -0600

    se termina 242 y 243 formulario sy validaciones dinamicas

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-page.component.css'
 })
 export class DynamicPageComponent {
+
+  public myForm:FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Spartan Total Warrior', Validators.required],
+      ['DefJam Figth For NY', Validators.required]
+    ])
+  });
+
+  constructor(private fb:FormBuilder) { }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField(field:string):boolean|null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray(formArray:FormArray, index:number):boolean|null {
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+
+  getFieldError(field:string):string|null {
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este  campo es requerido.';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`
+      }
+    }
+
+    return null;
+  }
+
+  onSubmit():void {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }

e3f262742e96191b9a8d2056c2d2bb85b603fdb4
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-27 08:27:54 -0600

    se termina 252 formulario de registro

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  public myForm:FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required ]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]]
+  });
+
+  constructor(private fb:FormBuilder) { }
+
+  isValidField(field:string):boolean {
+    // TODO: obtener de servicio
+    return 1===1;
+  }
+
+  onSubmit():void {
+    this.myForm.markAllAsTouched();
+  }
 
 }

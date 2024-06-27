b9641b65e5000a39380850f45db1e2a47e1ff798
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-27 10:04:15 -0600

    se termina 257 validador del correo

--- 07-formsApp/src/app/shared/validators/email-validator.service.ts
+++ 07-formsApp/src/app/shared/validators/email-validator.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { Observable, delay, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailValidator implements AsyncValidator {
 
+  // validate(control: AbstractControl):Observable<ValidationErrors | null> {
+  //   const email:string = control.value;
+
+  //   console.log({email});
+
+  //   return of({
+  //     emailTaken: true
+  //   });
+  // }
+
   validate(control: AbstractControl):Observable<ValidationErrors | null> {
     const email:string = control.value;
 
-    console.log({email});
+    const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+      if(email === 'jose@gmail.com') {
+        subscriber.next({emailTaken: true})
+        subscriber.complete()
+      }
 
-    return of({
-      emailTaken: true
-    });
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe(
+      delay(3000)
+    )
+
+    return httpCallObservable;
   }
 
 }

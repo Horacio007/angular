69e52f27d77c0eb8583efbedb0c271ea3ed3381d
DesarrolloPLS <desarrollo@plasalle.mx>
2024-06-27 15:04:37 -0600

    se termina 272 tercer selector anidado

--- 08-selectores/src/app/countries/services/countries.service.ts
+++ 08-selectores/src/app/countries/services/countries.service.ts
@@ -30,4 +30,18 @@ export class CountriesService {
       })))
     );
   }
+
+  getCountryByAlphaCode(alphaCode:string):Observable<SmallCountry> {
+
+    const url:string = `${this.baseURL}/alpha/${alphaCode}?fields=cca3,name,borders`;
+
+    return this._http.get<Country>(url).pipe(
+      map(country => ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders
+      }))
+    );
+  }
+
 }

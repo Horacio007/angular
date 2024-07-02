221572056c5dfb3b48cb5c4d620ab0fdb0d92a71
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-02 10:09:29 -0600

    se termina 309 standalone como componente reutilizable

--- 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })

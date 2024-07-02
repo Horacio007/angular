9e0d7d3e1c9461a276077e94a33a66b8b15e36b6
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-02 09:27:16 -0600

    se termina 303 componente mini mapa

--- 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
+++ 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat?:[number,number];
+  @ViewChild('map') divMap?:ElementRef;
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw "Map Div not found";
+
+    if(!this.lngLat) throw "LngLat can't be null"
+
+    const map = new Map({
+      //accessToken: mapboxgl.accessToken = 'pk.eyJ1IjoiaG9yYWNpby1nb25nb3JhIiwiYSI6ImNseTM1dTQ0OTA0YW4yanEyNXRjamZobmYifQ.5vWnRAKbUBU4_F0fWwWEZw',
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom,
+      interactive: false
+    });
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const marker = new Marker({
+      color: color
+    }).setLngLat(this.lngLat)
+    .addTo(map);
+  }
+
 
 }

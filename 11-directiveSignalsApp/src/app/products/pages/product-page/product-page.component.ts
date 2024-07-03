c16c08f2d4b1e55ab55e5a5bcc8ed58556a999da
DesarrolloPLS <desarrollo@plasalle.mx>
2024-07-03 10:29:15 -0600

    se termina 318 formulario reactivo tradicional

--- 11-directiveSignalsApp/src/app/products/pages/product-page/product-page.component.ts
+++ 11-directiveSignalsApp/src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Color } from '../../../../../../05-pipesApp/src/app/products/interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public color:string = 'green';
+
+  public myForm:FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(6), Validators.email]]
+  });
+
+  changeColor():void {
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+  }
+
+  // constructor(private fb:FormBuilder) { }
 
 }
